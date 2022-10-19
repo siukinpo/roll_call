d7f135186ad3170445f7d7af6a76b1b15e40cd95
James Siu <siukinpo@gmail.com>
2022-10-19 11:41:04 +0800

    second commit

--- roll_call.ts
+++ roll_call.ts
@@ -23,7 +23,32 @@ async function autoRollCall() {
     await popup.click("input#kc-login.btn.btn-primary.btn-block.btn-lg");
   });
 
+  await page
+    .locator("button.p-3.d-flex.align-items-center.btn.btn-success")
+    .click();
   await page.click("button.p-3.d-flex.align-items-center.btn.btn-primary");
+
+  // await page.click("button.p-3.d-flex.align-items-center.btn.btn-success");
+  // await page.click("button.p-3.d-flex.align-items-center.btn.btn-primary");
+  // let choices = await page.evaluate(async () => {
+  //   if (
+  //     document.querySelector(
+  //       "button.p-3.d-flex.align-items-center.btn.btn-primary"
+  //     )
+  //   ) {
+  //   } else {
+  //   }
+  // });
+
+  // if (
+  //   !document.querySelector(
+  //     "button.p-3.d-flex.align-items-center.btn.btn-primary"
+  //   )
+  // ) {
+  //   await page.click("button.p-3.d-flex.align-items-center.btn.btn-success");
+  // } else {
+  //   await page.click("button.p-3.d-flex.align-items-center.btn.btn-primary");
+  // }
 
   // page.on("popup", async (popup) => {
   //   await popup.waitForLoadState();

ea12aab403c80b6c9ad0e29ebc3c49428cbf3628
James Siu <siukinpo@gmail.com>
2022-10-19 11:54:08 +0800

    adjusted

--- roll_call.ts
+++ roll_call.ts
@@ -25,42 +25,6 @@ async function autoRollCall() {
     .locator("button.p-3.d-flex.align-items-center.btn.btn-success")
     .click();
   await page.click("button.p-3.d-flex.align-items-center.btn.btn-primary");
-
-  // await page.click("button.p-3.d-flex.align-items-center.btn.btn-success");
-  // await page.click("button.p-3.d-flex.align-items-center.btn.btn-primary");
-  // let choices = await page.evaluate(async () => {
-  //   if (
-  //     document.querySelector(
-  //       "button.p-3.d-flex.align-items-center.btn.btn-primary"
-  //     )
-  //   ) {
-  //   } else {
-  //   }
-  // });
-
-  // if (
-  //   !document.querySelector(
-  //     "button.p-3.d-flex.align-items-center.btn.btn-primary"
-  //   )
-  // ) {
-  //   await page.click("button.p-3.d-flex.align-items-center.btn.btn-success");
-  // } else {
-  //   await page.click("button.p-3.d-flex.align-items-center.btn.btn-primary");
-  // }
-
-  // page.on("popup", async (popup) => {
-  //   await popup.waitForLoadState();
-  //   await popup.title();
-  //   await popup.type(
-  //     "input#login_field.form-control.input-block.js-login-field",
-  //     "siukinpo@gmail.com"
-  //   );
-  //   await popup.type(
-  //     "input#password.form-control.input-block.js-password-field",
-  //     "357159Pet"
-  //   );
-  //   await popup.click("input.btn.btn-primary.btn-block.js-sign-in-button");
-  // });
 }
 
 autoRollCall();
